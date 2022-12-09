6b48e2c6ce5aa2097441900cbbefdaa2caaf4d42
guilhermeparente17 <guilherme.parente17@outlook.com>
2022-12-08 23:05:49 -0300

    Adicionando component Section

--- src/components/Section/Section.Elements.js
+++ src/components/Section/Section.Elements.js
@@ -1,44 +1,96 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const SectionContainer = styled.section`
-    width: 100%;
-    height: 100%;
-    padding: 50px 0px;
-    background: linear-gradient(#0A0B1F 25%, rgba(10,11,31,0.9) 50%);
-`;
-
-export const SectionImg = styled.img`
-    width: 375px;
-    height: 500px;
-    background-position: center;
-    background-size: cover;
-    margin-right: 50px;
-    border-radius: 10px;
-`;
-
-export const SectionContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    color: #fff;
-`;
-
-export const SecTitle = styled.h1`
-    font-size: 55px;
-    letter-spacing: 5px;
-    font-weight: 500;
-    margin-bottom: 20px;
-    text-shadow: 0.1em 0.1em 0.2em black;
-`;
-
-export const SecP = styled.p`
-    color: rgba(250,250,250, 0.7);
-    margin-bottom: 30px;
-    letter-spacing: 3px;
-`;
-
-export const SectionWrapper = styled.div`
-    padding: 50px;
+export const SectionContainer = styled.div`
+    background: #0c0c0c;
     display: flex;
     justify-content: center;
     align-items: center;
+    padding: 0 30px;
+    height: 400px;
+    position: relative;
+    z-index: 1;
+    
+    :before {
+        content: '';
+        position: absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom: 0;
+        background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%,
+        rgba(0,0,0,0.2) 100%),
+        linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    } 
+`
+
+export const HeroBg = styled.div`
+    position: absolute;
+    top:0;
+    right: 0;
+    bottom:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+`
+
+export const VideoBg = styled.video`
+    width: 100%;
+    height:100%;
+    -o-object-fit:cover;
+    object-fit: cover;
+    background: #232a34;
+`
+
+export const HeroContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const HeroH1 = styled.h1`
+    color: #fff;
+    font-size: 38px;
+    text-align: center;
+    font-weight: 500;
+    text-shadow: 3px 3px rgba(0,0,0,0.7);
+    @media screen and (max-width: 768px){
+        font-size: 40px;
+    }
+    @media screen and (max-width: 480px){
+        font-size: 32px;
+    }
+`
+
+export const HeroP = styled.p`
+    margin-top: 24px;
+    color: #fff;
+    font-size: 24px;
+    text-align: center;
+    max-width: 600px;
+    text-shadow: 3px 3px rgba(0,0,0,0.7);
+    @media screen and (max-width: 768px){
+        font-size: 24;
+    }
+    @media screen and (max-width: 480px){
+        font-size: 18px;
+    }
+`
+
+export const HeroBtnWrapper = styled.div`
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const HeroSpan = styled.span`
+    color: #F87529;
+    font-weight: bold;
+    text-shadow: 3px 3px rgba(0,0,0,0.7);
 `;
--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,39 +1,53 @@
 import React from 'react'
 
 import {
+    HeroBg,
+    HeroBtnWrapper,
+    HeroContent,
+    HeroH1,
+    HeroP,
     SectionContainer,
-    SectionImg,
-    SectionContent,
-    SecTitle,
-    SecP,
-    SectionWrapper
+    VideoBg,
+    HeroSpan
 } from './Section.Elements'
 
-import Image from '../../assets/images/bg2.jpg';
-import Comments from '../Comments';
-import { dataComments } from '../../dataComments';
+import Video from '../../assets/videos/video.mp4';
+import Lottie from 'react-lottie';
+import ArrowBottom from '../../assets/lotties/96084-arrow.json';
+
 
 const Section = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: ArrowBottom,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
   return (
     <SectionContainer>
-        <SectionWrapper>
-            <SectionImg src={Image} />
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+        </HeroBg>
 
-            <SectionContent>
-                <SecTitle>Our Key Features</SecTitle>
-
-                <SecP>
-                    We always provider the best service and features to out customers make <br /> 
-                    them happy when they buy art from out website.
-                </SecP>
-
-                {dataComments.map((item,index) => {
-                    return (
-                        <Comments key={index} item={item} />
-                    )
-                })}
-            </SectionContent>
-        </SectionWrapper>
+        <HeroContent>
+            <HeroH1>Discover rare
+                abstract paiting
+                and collect <HeroSpan>NFTs</HeroSpan>
+            </HeroH1>
+            <HeroP>
+                discover new pictures for your profile
+            </HeroP>
+            <HeroBtnWrapper>
+                <Lottie 
+                    options={defaultOptions}
+                    height={120}
+                    width={120}
+                />
+            </HeroBtnWrapper>
+            </HeroContent>
+        
     </SectionContainer>
   )
 }
